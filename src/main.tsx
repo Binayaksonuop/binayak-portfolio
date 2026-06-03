ec417cc22ea6826eb6b5e219213d44ce19a4eddd
Binayak Maharana <binayak@a4conserv.com>
2026-06-03 14:34:27 +0530

    fix: suppress THREE.Clock deprecation warning from react-three-fiber

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Suppress THREE.Clock deprecation warning from @react-three/fiber
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (typeof args[0] === 'string' && args[0].includes('THREE.Clock: This module has been deprecated')) {
+    return;
+  }
+  originalWarn(...args);
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
